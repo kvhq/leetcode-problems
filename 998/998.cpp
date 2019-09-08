e6dbafb6b21391fb3fdda485d9f80fb3eb1bdcf9
kvhq <kvhqll@gmail.com>
2019-09-08 22:08:02 +0300

    removed folders

--- 998/998.cpp
+++
@@ -1,9 +0,0 @@
-TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
-    if (!root || root->val < val) {
-        TreeNode* nRoot = new TreeNode(val);
-        nRoot->left = root;
-        return nRoot;
-    }
-    root->right = insertIntoMaxTree(root->right, val);
-    return root;
-}

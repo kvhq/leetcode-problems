d0c36a0da71dd9e3c12ac38520c2660f3ac9a8f6
kvhq <kvhqll@gmail.com>
2020-06-10 18:53:59 +0300

    problem 617 (new solution)

--- problems/easy/617.cpp
+++ problems/easy/617.cpp
@@ -1,3 +1,28 @@
+// Number: 617
+// Name: Merge Two Binary Trees
+// Tags: bst, stack, recursion
+
+// recursion
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+    if (t1 == nullptr) return t2;
+    if (t2 == nullptr) return t1;
+    t1->val = t1->val + t2->val;
+    if (t1->right && t2->right) {
+        t1->right = mergeTrees(t1->right, t2->right);
+    }
+    if (!t1->right && t2->right) {
+        t1->right = t2->right;
+    }
+    if (t1->left && t2->left) {
+        t1->left = mergeTrees(t1->left, t2->left);
+    }
+    if (!t1->left && t2->left) {
+        t1->left = t2->left;
+    }
+    return t1;
+}
+
+// stack
 TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
     if (!t2) return t1;
     if (!t1) return t2;

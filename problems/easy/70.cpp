8f8b6efebd88c231f02f7df0b9e5970dbfef217f
kovvas <kvhqll@gmail.com>
2021-07-07 13:04:52 +0300

    modified 70

--- problems/easy/70.cpp
+++ problems/easy/70.cpp
@@ -1,77 +1,22 @@
 // Number: 70
 // Name: Climbing Stairs
-// Tags: DP
+// Tags: memoization
 
-// brute force
-class Solution_0 {
-public:
-    int climbStairs(int n) {
-        if (n == 0) return 1;
-        int answer = 0;
-        for (int i = 1; i <= 2; ++i) {
-            if (n - i >= 0) answer += climbStairs(n - i);
-        }
-        return answer;
+int helpFunc(int floor, int hight, vector<int>& memo) {
+    if (floor > hight) {
+        return 0;
     }
-};
+    if (floor == hight) {
+        return 1;
+    }
+    if (memo[floor] > 0) {
+        return memo[floor];
+    }
+    memo[floor] = helpFunc(floor + 1, hight, memo) + helpFunc(floor + 2, hight, memo);
+    return memo[floor];
+}
 
-// DP top-down
-class Solution_1 {
-public:
-    int helper(int i_n, vector<int>& i_cache) {
-        if (i_n == 0) return 1;
-        int answer = 0;
-        for (int i = 1; i <= 2; ++i) {
-            if (i_n - i >= 0) {
-                if (i_cache[i_n - i] != -1) answer += i_cache[i_n - i];
-                else {
-                    answer += helper(i_n - 1, i_cache);
-                    i_cache[i_n - i] = answer;
-                }
-            }
-        }
-        return answer;
-    }
-    
-    int climbStairs(int n) {
-        vector<int> cache(n, -1);
-        int answer = helper(n, cache);
-        return answer;
-    }
-};
-
-// bottom-up
-class Solution_2 {
-public:
-    int climbStairs(int n) {
-        vector<int> cache(n + 1, -1);
-        cache[0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            if (cache[i] == -1) {
-                int cur_combs = 0;
-                for (int j = 1; j <= 2; ++j) {
-                    if (i - j >= 0) cur_combs += cache[i - j];
-                }
-                cache[i] = cur_combs;
-            }
-        }
-        return cache.back();
-    }
-};
-
-// O(1) memory
-class Solution_3 {
-public:
-    int climbStairs(int n) {
-        if (n == 1) return 1;
-        int first = 1;
-        int second = 2;
-        for (int i = 3; i <= n; ++i) {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-        return second;
-    }
-};
-
+int climbStairs(int n) {
+    vector<int> memo(n);
+    return helpFunc(0, n, memo);
+}

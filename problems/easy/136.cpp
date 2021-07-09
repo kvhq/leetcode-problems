6db6ac3dc7c7ab3add615d476dd41350d3ad49aa
kovvas <vasili.kovalyov@gmail.com>
2021-07-09 13:25:52 +0300

    136

--- problems/easy/136.cpp
+++ problems/easy/136.cpp
@@ -17,3 +17,16 @@ int singleNumber(vector<int>& nums) {
 
 //should be done one more solution
 //using bit manipulation
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        int answer = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            answer ^= nums[i];
+        }
+        return answer;
+    }
+};
+
